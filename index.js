8e4e630c43f121430cf4e6fa3ffd545ce632db3e
jgonzalez <jereegonzalez@gmail.com>
2020-05-29 23:08:31 -0300

    Updating project components

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 /**
  * @format
  */
-
+import React from 'react';
+import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
-import App from './App';
 import {name as appName} from './app.json';
+import AlbumList from './src/components/AlbumList';
+import PhotoList from './src/components/PhotoList';
+import {Router, Scene, Stack} from 'react-native-router-flux';
+
+// Create a component
+const App = () => (
+  <Router>
+    <Stack key="root">
+      <Scene
+        key="albumList"
+        component={AlbumList}
+        title="Albums"
+        initial={true}
+      />
+      <Scene key="photoList" component={PhotoList} title="Photos" />
+    </Stack>
+  </Router>
+);
 
 AppRegistry.registerComponent(appName, () => App);
