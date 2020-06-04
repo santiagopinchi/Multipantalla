195e855ad511ef32da510bc9a339ed263693755f
santiagopinchi <santiagopinchi@gmail.com>
2020-06-04 20:13:28 -0300

    styles

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,14 +1,28 @@
 import React from 'react';
-import { Text, View} from 'react-native';
+import { ActivityIndicator, StyleSheet, View} from 'react-native';
 
 const Loading = () => {
     return ( 
-        <View style={{ flex: 1 }}>
-			<Text>
-                {'loading'}
-			</Text>
+        <View style={[ styles.container, styles.horizontal ]}>
+            <ActivityIndicator size="large" color="#0000ff" />
+            <ActivityIndicator size="small" color="#00ff00" />
+            <ActivityIndicator size="large" color="#0000ff" />
+            <ActivityIndicator size="small" color="#00ff00" />
         </View>
      );
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center"
+    },
+    horizontal: {
+      flexDirection: "row",
+      justifyContent: "space-around",
+      padding: 10
+    }
+  });
  
 export default Loading;
+
--- src/components/NotFoundError.js
+++ src/components/NotFoundError.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { Text, View, Image} from 'react-native';
+import { View, Image, StyleSheet} from 'react-native';
 
 const NotFound = () => {
     return ( 
-        <View style={{ flex: 1 }}>
-			<Text>
-                {'ERROR AL CARGAR DATOS'}
-			</Text>
+        <View style={[ styles.container ]}>			
             <Image 
                 source={ require('../images/error.png') }
             />
         </View>
      );
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center"
+    }
+});
  
 export default NotFound;
